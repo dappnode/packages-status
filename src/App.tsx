538ce10f32a4e84d15188baf4187681ba08a3963
Pablo Mendez <mendez4a@gmail.com>
2023-05-15 14:20:43 +0200

    trigger action

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          packages status
+          packages statuss
         </a>
       </header>
     </div>
