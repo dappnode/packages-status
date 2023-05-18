4e0f9afc6e37bf45f73468b62126c1e546a6fb45
Pablo Mendez <mendez4a@gmail.com>
2023-05-18 17:42:07 +0200

    fix linter

--- src/TopBar.tsx
+++ src/TopBar.tsx
@@ -76,6 +76,7 @@ export default function TopBar({
       });
       setFilteredRows(filteredData);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchQuery]);
 
   return (
